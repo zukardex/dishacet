54cfbd7fb9f1d9f74e2b10deb988e73560ea2a7c
Mohammed Suhail K M <zenzenram@gmail.com>
2023-12-23 00:21:10 +0530

    Create credentials.php

---
+++ coordinator/credentials.php
@@ -0,0 +1,17 @@
+<?php    
+//<NOTES>
+        //group=>1 implies it's a group event
+        /*
+            ar= architecture
+            ce= civil
+            cs= computer science
+            ec = AEI/ECE
+            ee= electrical
+            ie= industrial
+            me= mechanical
+        */
+//</NOTES>
+$users = array(
+        //array('name' => "name", 'password' => "34YU4OUH",'event'=>"Skit", 'group'=>1),
+    );
+?>
