552f216faee22a774d2491f0bc6945fce3bb3e29
Mohammed Suhail K M <zenzenram@gmail.com>
2023-12-23 00:40:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <h1> Welcome to DISHA'23 Dashboard</h1>
 <a href="coordinators/login.php">Login as a coordinator</a>
 <a href="core/login.php">Login for Convenor/Core Committee</a>
-<a href="coordinators/login.php">Login as a coordinator</a>
+<a href="coordinator/login.php">Login as a coordinator</a>
 <iframe width="100%" src="score/index.html" height="500"></iframe>
