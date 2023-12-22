17585c42725688582d63af00598c04adcc83e610
Mohammed Suhail K M <zenzenram@gmail.com>
2023-12-23 00:32:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 <a href="coordinators/login.php">Login as a coordinator</a>
 <a href="core/login.php">Login for Convenor/Core Committee</a>
 <a href="coordinators/login.php">Login as a coordinator</a>
-<iframe width="100%" src="score/home.html" height="500"></iframe>
+<iframe width="100%" src="score/index.html" height="500"></iframe>
