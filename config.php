e47f72e97137d67d979930be55da5f9e7e097716
Mohammed Suhail K M <zenzenram@gmail.com>
2023-12-23 00:20:21 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$db_host="";
+$db_user="";
+$db_pass="";
+$db_name="dishacet";
+$conn= new mysqli($db_host, $db_user, $db_pass, $db_name);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
